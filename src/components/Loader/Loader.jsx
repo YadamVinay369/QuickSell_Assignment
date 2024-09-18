b770834d8dfe7ab2c1b92d1a3d93326444accf6f
YadamVinay369 <vinay.y21@iiits.in>
2024-09-18 22:57:46 +0530

    feat: Loader updated

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { FidgetSpinner } from "react-loader-spinner";
 
-const Loader = () => {
-  return <div></div>;
+const Loading = () => {
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        gap: "5px",
+      }}
+    >
+      <FidgetSpinner
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="fidget-spinner-loading"
+        wrapperStyle={{}}
+        wrapperClass="fidget-spinner-wrapper"
+      />{" "}
+      <span
+        style={{
+          color: "green",
+          fontWeight: "bolder",
+          letterSpacing: "2px",
+        }}
+      >
+        QuickSell Assignment
+      </span>
+    </div>
+  );
 };
 
-export default Loader;
+export default Loading;
